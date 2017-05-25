06e4e8759acc0d1389c64a1a6f3c3d5a83d9cc63
Kim kyuna <kkn00077@gmail.com>
2017-05-25 20:49:37 +0900

    #3 itemlist and Event

--- app/src/main/java/kn0077/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/kn0077/kr/hs/emirim/dialogtest/MainActivity.java
@@ -1,5 +1,7 @@
 package kn0077.kr.hs.emirim.dialogtest;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,16 +9,30 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    String[]itemArr={"바나나","딸기","망고","청포도"};//onClick 메서드 안에 선언해도 됨
+    Button butDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button)findViewById(R.id.but_dialog);
+        butDialog=(Button)findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this); // 대화상자 객체 생성과 동시에 어느 화면에 띄워 줄 것인지 지정
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.mipmap.star);
+        //dialog.setMessage("여기는 메시지를 쓰는 곳입니다. ");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) { // which 는 클릭한 항목의 인덱스값
+                butDialog.setText(itemArr[which]);
+            }
+        });
+        dialog.setPositiveButton("OK",null);
+        dialog.show();//다이얼로그를 화면에 보이게 설정
     }
 }
